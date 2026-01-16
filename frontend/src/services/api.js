7bdcee266982950e2e608eec1136a48a6711775f
MADALA LEELA VENKATA SIVA SPOORTHI <mmadalavenkatesh@gmail.com>
2026-01-16 11:57:05 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://todo-list-app-9dhd.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
@@ -56,4 +56,5 @@ export const taskAPI = {
 };
 
 export default api;
+
 
